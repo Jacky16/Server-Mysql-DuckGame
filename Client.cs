9053d931b3e8be3edffdc3c231ce405f50c90502
Adria Martinez <adriamartinez94@gmail.com>
2022-04-25 13:27:16 +0200

    Cambiado a codigo de alex

--- Client.cs
+++ Client.cs
@@ -1,18 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Sockets;
 
+//Clase para almacenar los datos necesarios de cada cliente
 class Client
 {
-    private TcpClient tcp;
-    private string nick;
 
+    private TcpClient tcp; //Almacenamos los datos de conexion del cliente
+    private string nick; //Almacenamos el nick del cliente (en este caso hardcodeado)
+    private bool waitingPing; //Almacenamos si estamos a la espera que responda un ping de conexion
+
+    //Constructor de la clase
     public Client(TcpClient tcp)
     {
         this.tcp = tcp;
         this.nick = "Anonymous";
+        this.waitingPing = false;
+    }
+
+    //-------------------------------- Getters ----------------------
+    public TcpClient GetTcpClient()
+    {
+        return this.tcp;
+    }
+
+    public string GetNick()
+    {
+        return this.nick;
+    }
+
+    public bool GetWaitingPing()
+    {
+        return this.waitingPing;
+    }
+
+    //-------------------------------- Setters ----------------------
+    public void SetWaitingPing(bool waitingPing)
+    {
+        this.waitingPing = waitingPing;
     }
 }
